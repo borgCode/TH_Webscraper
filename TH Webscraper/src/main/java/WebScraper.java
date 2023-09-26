dc7f37b80ecf2325d65af256e7f0a05b029fd445
hyper <123260045+KieranCode94@users.noreply.github.com>
2023-09-26 13:49:01 +0200

    scrape all members

--- TH Webscraper/src/main/java/WebScraper.java
+++ TH Webscraper/src/main/java/WebScraper.java
@@ -14,24 +14,31 @@ public class WebScraper {
 
     public static void main(String[] args) throws IOException {
 
-
-        Document doc = Jsoup.connect("https://www.teamhitless.com/project/shilkey10/").get();
-        Element title = doc.select("#et-boc > div > div > div > div.et_pb_row.et_pb_row_0 > div > div > div > h1 > span").first();
-        Element title2 = doc.select("#et-boc > div > div > div > div.et_pb_row.et_pb_row_0 > div > div > div > h1").first();
-        Elements body = doc.select("#et-boc > div > div > div > div.et_pb_row.et_pb_row_2.et_pb_equal_columns.et_pb_gutters4 > div.et_pb_column.et_pb_column_2_3.et_pb_column_2.et_pb_css_mix_blend_mode_passthrough");
-        Elements body2 = doc.select("#et-boc > div > div > div > div.et_pb_row.et_pb_row_2.et_pb_equal_columns.et_pb_gutters4 > div > div");
-
-        String runner = title != null ? title.ownText() : title2 != null ? title2.ownText() : "Runner not found";
-
         String filePath = "runs.csv";
         BufferedWriter writer = Files.newBufferedWriter(Paths.get(filePath), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
         CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
 
-        if (body.select("p").isEmpty()) {
-            saveRecord(csvPrinter, runner, body2);
+        Document document = Jsoup.connect("https://www.teamhitless.com/about/members/").get();
+        Elements runnerBody = document.select("#et-boc > div > div > div.et_pb_section.et_pb_section_1.et_pb_with_background.et_section_regular > div.et_pb_row.et_pb_row_2 > div > div > div > p").select("a");
+        for (int i = 0; i < runnerBody.size(); i++) {
+            String urlString = getURL(runnerBody.get(i).attr("href"));
+            Document doc = Jsoup.parse(urlString);
+            doc.connection().get();
+            Element title = doc.select("#et-boc > div > div > div > div.et_pb_row.et_pb_row_0 > div > div > div > h1 > span").first();
+            Element title2 = doc.select("#et-boc > div > div > div > div.et_pb_row.et_pb_row_0 > div > div > div > h1").first();
+            Elements body = doc.select("#et-boc > div > div > div > div.et_pb_row.et_pb_row_2.et_pb_equal_columns.et_pb_gutters4 > div.et_pb_column.et_pb_column_2_3.et_pb_column_2.et_pb_css_mix_blend_mode_passthrough");
+            Elements body2 = doc.select("#et-boc > div > div > div > div.et_pb_row.et_pb_row_2.et_pb_equal_columns.et_pb_gutters4 > div > div");
+
+            String runner = title != null ? title.ownText() : title2 != null ? title2.ownText() : "Runner not found";
+
+            if (body.select("p").isEmpty()) {
+                saveRecord(csvPrinter, runner, body2);
+            }
+            saveRecord(csvPrinter, runner, body);
+            csvPrinter.flush();
         }
-        saveRecord(csvPrinter, runner, body);
-        csvPrinter.flush();
+
+
 
     }
 
@@ -57,5 +64,10 @@ public class WebScraper {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String getURL(String href) {
+        String url = "https://www.teamhitless.com" + href;
+        return url;
     }
 }
